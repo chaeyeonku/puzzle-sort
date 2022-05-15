5f9501bd2b3c369dafe92ca37272b4d1104f2359
chaeyeonku <applekoo63@gmail.com>
2022-05-14 23:37:10 -0700

    Added animation to soritng process

--- src/components/puzzle.jsx
+++ src/components/puzzle.jsx
@@ -23,7 +23,7 @@ class Puzzle extends React.Component {
             // initialize puzzle information
             this.setState(
                 {width: img.width, height: img.height, 
-                    numOfPieces: img.width * img.height}
+                    numOfPieces: 16}
             );
         };
 
@@ -31,6 +31,8 @@ class Puzzle extends React.Component {
 
     handleShuffle = () => {
         console.log("Clicked Shuffle");
+
+        let sizeOfPiece = 64;
 
         // hide original canvas
         var origCanvas = document.getElementById("myCanvas");
@@ -58,18 +60,6 @@ class Puzzle extends React.Component {
 
         let x, y, newX, newY;
 
-        // render shuffled puzzle on canvas
-        // for (let i = 0; i < this.state.numOfPieces; i++) {
-        //     x = initialArray[i] % this.state.width;
-        //     y = initialArray[i] / this.state.width;
-
-        //     newX = i % this.state.width;
-        //     newY = i / this.state.width;
-
-        //     var imgData = ctx.getImageData(x, y, 1, 1);
-        //     ctx2.putImageData(imgData, newX, newY);
-        // }
-
         // animated shuffle
         let count = 0;
         let max = this.state.numOfPieces;
@@ -77,14 +67,14 @@ class Puzzle extends React.Component {
         const id = setInterval(drawPiece, 10);
         function drawPiece() {
             if ( count < max-1 ) {
-                for (let i = 0; i < width; i++) {
-                    x = initialArray[count] % width;
-                    y = initialArray[count] / width;
+                for (let i = 0; i < width / 64; i++) {
+                    x = initialArray[count] % width * 64;
+                    y = initialArray[count] / width * 64;
 
-                    newX = count % width;
-                    newY = count / width;
+                    newX = count % width * 64;
+                    newY = count / width / 64;
 
-                    var imgData = ctx.getImageData(x, y, 1, 1);
+                    var imgData = ctx.getImageData(x, y, sizeOfPiece, sizeOfPiece);
                     ctx2.putImageData(imgData, newX, newY);
 
                     count++;
@@ -94,15 +84,33 @@ class Puzzle extends React.Component {
                 clearInterval(id);
             }
         }
+
     };
 
     handleSort = () => {
         console.log("Clicked Sort");
         
         let array = this.state.array;
+        let width = this.state.width;
         let numOfPieces = this.state.numOfPieces;
 
-        quicksort(array, 0, numOfPieces-1);
+        let c = document.getElementById("myCanvas");
+        let ctx = c.getContext("2d");
+        let c2 = document.getElementById("myCanvas2");
+        let ctx2 = c2.getContext("2d");
+
+        let low = 0;
+        let high = numOfPieces - 1;
+        let pivotIdx = -1;
+
+        // initial call to quicksort
+        quicksort(array, low, high);
+
+        // helper handling recursive calls to quicksort
+        function recursiveQuickSort() {
+            quicksort(array, low, pivotIdx - 1);
+            quicksort(array, pivotIdx + 1, high);
+        }
 
         // helper that picks a pivot and sorts other elements
         function partition(array, low, high) {
@@ -126,35 +134,66 @@ class Puzzle extends React.Component {
 
         // do quicksort
         function quicksort(array, low, high) {
-            if (low < high) {
-                let pivot = partition(array, low, high);
 
-                quicksort(array, low, pivot - 1);
-                quicksort(array, pivot + 1, high);
+            if (low < high) {
+                pivotIdx = partition(array, low, high);
+                // let pivot = partition(array, low, high);
+
+                // update canvas
+                drawCurrentPuzzle();
+                window.requestAnimationFrame(recursiveQuickSort);
+                // quicksort(array, low, pivot - 1);
+                // quicksort(array, pivot + 1, high);
+
+                // drawCurrentPuzzle()
+                // .then(quicksort(array, low, pivot - 1))
+                // .then(quicksort(array, pivot + 1, high));
+
+                // quicksort(array, low, pivot - 1)
+                // .then(quicksort(array, pivot + 1, high));
 
             }
+
+            
         };
+
+        // animate function
+        function drawCurrentPuzzle() {
+            let x, y, newX, newY;
+
+            for (let i = 0; i < numOfPieces; i++) {
+                x = array[i] % width;
+                y = array[i] / width;
+    
+                newX = i % width;
+                newY = i / width;
+    
+                var imgData = ctx.getImageData(x, y, 1, 1);
+                ctx2.putImageData(imgData, newX, newY);
+            }
+            
+        }
 
         console.log(array);
 
-        let c = document.getElementById("myCanvas");
-        let ctx = c.getContext("2d");
-        let c2 = document.getElementById("myCanvas2");
-        let ctx2 = c2.getContext("2d");
+        // let c = document.getElementById("myCanvas");
+        // let ctx = c.getContext("2d");
+        // let c2 = document.getElementById("myCanvas2");
+        // let ctx2 = c2.getContext("2d");
 
-        let x, y, newX, newY;
+        // let x, y, newX, newY;
 
-        // render shuffled puzzle on canvas
-        for (let i = 0; i < numOfPieces; i++) {
-            x = array[i] % this.state.width;
-            y = array[i] / this.state.width;
+        // // sorted puzzle
+        // for (let i = 0; i < numOfPieces; i++) {
+        //     x = array[i] % this.state.width;
+        //     y = array[i] / this.state.width;
 
-            newX = i % this.state.width;
-            newY = i / this.state.width;
+        //     newX = i % this.state.width;
+        //     newY = i / this.state.width;
 
-            var imgData = ctx.getImageData(x, y, 1, 1);
-            ctx2.putImageData(imgData, newX, newY);
-        }
+        //     var imgData = ctx.getImageData(x, y, 1, 1);
+        //     ctx2.putImageData(imgData, newX, newY);
+        // }
 
     };
 
